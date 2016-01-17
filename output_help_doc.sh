8c0050b607880627686369f53bcce0775b17ba7c
MingquanLiang <shevnko@163.com>
2016-01-17 14:34:40 +0800

    make the script output sub-module

--- output_help_doc.sh
+++ output_help_doc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-module_name=$1
+check_module=$1
 if [ $# == 1 ]
 then
-    output_filename="${module_name}_help.txt"
+    output_filename="${check_module}_help.txt"
 elif [ $# == 2 ]
 then
     output_filename=$2
@@ -11,12 +11,20 @@ else
     echo "Usage: $0 module_name [output_filename]"
     exit 1
 fi
+
+if [[ ${check_module} =~ '.' ]]
+then
+    module_name=$(echo "${check_module}" | awk -F'.' '{print $1}')
+else
+    module_name=${check_module}
+fi
+
 python -c "import ${module_name}"
 ret=$?
 if [ "$ret" == "0" ]
 then
-    python -c "import ${module_name}; help($module_name)" > ${output_filename}
-    echo "The help doc of module $module_name is $output_filename"
+    python -c "import ${module_name}; help(${check_module})" > ${output_filename}
+    echo "The help doc of module ${check_module} is ${output_filename}"
 else
     echo "Failed to output the help doc of ${module_name}"
     exit 1
